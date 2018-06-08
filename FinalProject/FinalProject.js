2b1365973e43d4644b1d8ac3b370388d51790ffb
Erica Holland <erica.holland87@outlook.com>
2018-06-08 12:51:45 -0400

    added javascript

--- FinalProject/FinalProject.js
+++ FinalProject/FinalProject.js
@@ -3,3 +3,10 @@ $(function(){
         $(".main-nav").slideToggle();
     })
 })
+
+
+$(document).ready(function(){
+    $('.button').click(function(){
+        alert("Your information has not been submitted because I do not know back-end programming.");
+    });
+});
