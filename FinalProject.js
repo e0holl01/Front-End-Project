d27bd656bba322d1112814e5c419141fe268299a
e0holl01 <39104984+e0holl01@users.noreply.github.com>
2018-07-01 08:41:41 -0400

    Add files via upload

---
+++ FinalProject.js
@@ -0,0 +1,31 @@
+
+$(function(){
+    $('#flashMessage').hide();
+
+    $(".mobileToggle").click(function(){
+        $(".main-nav").slideToggle();
+    })
+
+    $('#submit').on("click",function() {
+        showMessage();
+    });
+
+    $(document).on('submit', function (event) {
+        event.preventDefault();
+        return false;
+    })
+
+ 
+ 
+
+
+})
+
+function showMessage() {
+    var form = $("form")[0]
+    if(form.checkValidity()) {
+        $('#flashMessage').slideDown(900).delay(3000).slideUp(900);
+    }
+}
+
+// need to reset form when submitted. message still needs to show
